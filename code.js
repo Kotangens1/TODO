1091604b1cefb021869db587ce3c714db911115c
Kate-loptop <shepenevakaterina@gmail.com>
2022-03-04 15:44:29 +0300

    input and add tasks

--- code.js
+++ code.js
@@ -1,1 +1,22 @@
 console.log("Файл .js подключён!");
+
+let todo__list = document.querySelector(".todo__list")
+let todo__new__input = document.querySelector(".todo__new__input");
+let btnAdd = document.querySelector(".btnAdd")
+let index = 0;
+let task_description = "ничего";
+
+const render = index => {
+    return `            
+    <li class="task" id = "task${index}">
+        <div class="task__description">${task_description}</div>
+    </li>
+    `
+}
+
+btnAdd.addEventListener('click', () => {
+    task_description = document.querySelector(".todo__new__input").value;
+    todo__list.innerHTML += render(index);
+    index ++;
+});
+
