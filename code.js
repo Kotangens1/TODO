5d605a2579149f64e5b5179a0029f44237a5b843
Kate-loptop <shepenevakaterina@gmail.com>
2022-03-06 19:27:32 +0300

    add checkbox and button delete

--- code.js
+++ code.js
@@ -1,22 +1,35 @@
-console.log("Файл .js подключён!");
+const todo__list = document.querySelector(".todo__list")
+const todo__input = document.querySelector(".todo__input");
+const btnAdd = document.querySelector(".btnAdd");
+let tasks = [];
 
-let todo__list = document.querySelector(".todo__list")
-let todo__new__input = document.querySelector(".todo__new__input");
-let btnAdd = document.querySelector(".btnAdd")
-let index = 0;
-let task_description = "ничего";
+function createTask() {
+    text = "";
+    id = Math.random();
+    isChecked = false;
+}
 
-const render = index => {
+function render(task)  {
+    task.text = todo__input.value;
+    task.id = Math.random();
+    task.isChecked = false;
     return `            
-    <li class="task" id = "task${index}">
-        <div class="task__description">${task_description}</div>
+    <li class="task" id = "task${task.id}">
+        <input class="btnCheck" type="checkbox" id= "done${task.id}"><label for="done${task.id}"></label>
+        <div class="task__description">${task.text}</div>
+        <button  class="btnDel"><img src="img/del.svg" alt="удалить"></button>
     </li>
-    `
+    `;
+    
+    
 }
 
 btnAdd.addEventListener('click', () => {
-    task_description = document.querySelector(".todo__new__input").value;
-    todo__list.innerHTML += render(index);
-    index ++;
+    var task = new createTask();
+    tasks += task;
+    console.log(task);
+
+    console.log(tasks);
+    todo__list.innerHTML += render(task);
 });
 
