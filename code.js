cf8be1fe07ff8fcf9e40fec2e944cf0457a58f74
Kate-loptop <shepenevakaterina@gmail.com>
2022-03-07 13:42:11 +0300

    fixed bags

--- code.js
+++ code.js
@@ -2,34 +2,31 @@ const todo__list = document.querySelector(".todo__list")
 const todo__input = document.querySelector(".todo__input");
 const btnAdd = document.querySelector(".btnAdd");
 let tasks = [];
+let task = 0;
 
 function createTask() {
-    text = "";
-    id = Math.random();
-    isChecked = false;
+    const task = {
+    text: todo__input.value,
+    id: Math.random(),
+    isChecked: false
+} 
+    return task;
 }
 
-function render(task)  {
-    task.text = todo__input.value;
-    task.id = Math.random();
-    task.isChecked = false;
-    return `            
+function render(tasks)  {
+    todo__list.innerHTML += 
+    `            
     <li class="task" id = "task${task.id}">
         <input class="btnCheck" type="checkbox" id= "done${task.id}"><label for="done${task.id}"></label>
         <div class="task__description">${task.text}</div>
         <button  class="btnDel"><img src="img/del.svg" alt="удалить"></button>
     </li>
     `;
-    
-    
 }
 
 btnAdd.addEventListener('click', () => {
-    var task = new createTask();
+    task = createTask();
     tasks += task;
-    console.log(task);
-
-    console.log(tasks);
-    todo__list.innerHTML += render(task);
+    render(tasks);
 });
 
